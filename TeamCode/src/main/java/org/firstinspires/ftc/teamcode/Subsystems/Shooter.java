dffe290d47625defa3df72f79d1d579c616cabf8
Aaron The Awsome <vaz.nightwalker@gmail.com>
2026-05-28 10:49:20 +0200

    Clanker

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Config
+public class Shooter {
+
+    public static double TARGET_RPM = 500; // change this live from the dashboard
+    double gearRatio = 15.0/16.0;
+    public static double MOTOR_SPEED = 1;
+    public static double PICKUP_SPEED = 0.4;
+    DcMotorEx shooter1;
+    DcMotorEx shooter2;
+    DcMotorEx pickUp;
+    public static double forward = 0.5;
+    public static double back = 0.5;
+
+    Servo myServo;
+    Servo myServo2;
+
+    boolean running = false;
+
+    public Shooter(HardwareMap hardwareMap) {
+        shooter1 = hardwareMap.get(DcMotorEx.class, "shooter1");
+        shooter2 = hardwareMap.get(DcMotorEx.class, "shooter2");
+        pickUp = hardwareMap.get(DcMotorEx.class, "pickUp");
+
+        shooter1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        shooter2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        pickUp.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pickUp.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        shooter1.setDirection(DcMotor.Direction.FORWARD);
+        shooter2.setDirection(DcMotorSimple.Direction.REVERSE);
+        pickUp.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        shooter1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooter2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        pickUp.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        //myServo =  hardwareMap.get(Servo.class, "myServo");
+        //myServo2 =  hardwareMap.get(Servo.class, "myServo2");
+
+    }
+
+    public void togglePickUp(){
+        if (running){
+            pickUp.setPower(0);
+            running = false;
+        }else {
+            pickUp.setPower(PICKUP_SPEED);
+            running = true;
+        }
+    }
+    public void toggleMotor() {
+        if (running) {
+            //shooter1.setVelocity(0);
+            //shooter2.setVelocity(0);
+            shooter1.setPower(0);
+            shooter2.setPower(0);
+            running = false;
+        } else {
+            double targetDegPerSec = TARGET_RPM * 6.0 * gearRatio; // reads latest value from dashboard
+            //shooter1.setVelocity(targetDegPerSec, AngleUnit.DEGREES);
+            //shooter2.setVelocity(targetDegPerSec, AngleUnit.DEGREES);
+            shooter1.setPower(MOTOR_SPEED);
+            shooter2.setPower(MOTOR_SPEED);
+            running = true;
+        }
+    }
+    //public void servopos2() {
+   //     myServo.setPosition(0.5 + back);
+   //     myServo2.setPosition(0.5 - forward);
+    //}
+    //public void servopos1() {
+      //  myServo.setPosition(0.5);
+        //myServo2.setPosition(0.5);
+   // }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SuperSystem.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Config
+public class SuperSystem {
+    //declare objects
+    Telemetry dashboardTelemetry;
+    ElapsedTime clawTimer = new ElapsedTime();
+    public static int clawPause = 150;
+    ElapsedTime armTimer = new ElapsedTime();
+    boolean armPauseTriggered = false;
+    ElapsedTime transferTimer = new ElapsedTime();
+    boolean transferTriggered = false;
+    public static int transferPause = 800;
+    public static int pickUpPause = 350;
+    boolean isPickupPause = false;
+    ElapsedTime pickUpPauseTimer = new ElapsedTime();
+    ElapsedTime scanPause = new ElapsedTime();
+    boolean isScanning = false;
+    public static double scanPowerFast = 1; // 2/17 changed from .6
+    public static double scanPowerSlow = .4; // 2/17 changed from .3
+    public static double upperLimit = -.15; //-0.05 for clip
+    public static double lowerLimit = -0.22; // -0.15 for clip
+    public static double autoUpperLimit = -.13; //-0.05 for clip
+    public static double autoLowerLimit = -0.25; // -0.15 for clip
+
+    public static int xScanDirection = 0;
+    public boolean xReady = false;
+    public boolean yReady = false;
+    public static double xLeftLimit = -0.15;
+    public static double xRightLimit = 0.05;
+    public static int scanToggle;
+    public static boolean isLowScanning = false;
+    public boolean isLowPickupPause = false;
+    public boolean prepTransfer = false;
+    public boolean lowTransfer = false;
+
+    //set up vision
+    RevBlinkinLedDriver blinkin;
+    Servo rgbLED;
+    public static MyLimeLight myLimeLight;
+    DcMotorSimple headlights;
+    boolean isBlue = false;
+    int toggleState = 0; //0 is yellow, 1 is color, 2 is lift
+    boolean isAutoScan = false;
+    boolean isAutoTransferDone = false;
+    boolean isAutoClip = false;
+    boolean isAutoTransferPause = false;
+    boolean isAuto;
+    boolean isAutoBasket = false;
+
+    public SuperSystem(@NonNull HardwareMap hardwareMap, @NonNull Telemetry db, int program){
+        dashboardTelemetry = db;
+        headlights = hardwareMap.get(DcMotorSimple.class, "led");
+        myLimeLight = new MyLimeLight(hardwareMap);
+        blinkin = hardwareMap.get(RevBlinkinLedDriver.class, "blinken");
+        rgbLED = hardwareMap.get(Servo.class,"rgbLight");
+        blinkin.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+        rgbLED.setPosition(.333);
+//        isAuto = b;
+        if (program != 0) {
+            isAuto = true;
+            if (program == 1) {
+                upperLimit = autoUpperLimit; //-0.05 for clip
+                lowerLimit = autoLowerLimit; // -0.15 for clip
+                isAutoClip = true;
+            } else {
+                upperLimit = -.15;
+                lowerLimit = -.22;
+                isAutoBasket = true;
+            }
+        }
+    }
+
+    public void start(){
+        headlights.setPower(0);
+    }
+}
